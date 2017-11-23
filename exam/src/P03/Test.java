ab0f644d07ba128893487403bc9d5eebeb52a278
kmjy <jae080004@gmail.com>
2017-11-23 18:34:50 +0900

    afwe

--- exam/src/P03/Test.java
+++ exam/src/P03/Test.java
@@ -1,17 +1,19 @@
 package P03;
 
 public class Test {
+	String str;
 	
-	void print(String str) {
+	Test(String str){
+		str = str;
+	}
+	void print () {
 		System.out.println(str);
 	}
-	//raw.githubusercontent.com/cnfree/eclipse/master/decompiler/update/"
+
 public static void main (String[]args) {
-	Test t = new Test();
-	String str = " 나 여깄어요~~";
-	t.print(str);
-	t.print(str.replace("나","박경훈"));
-	t.print(str);
+	Test t = new Test("가나다abc123");
+	t.print();
+
 	}
 }
 
